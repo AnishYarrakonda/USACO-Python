ebb8154c6db10e5d35cc4c77b74f6dbbba4e278b
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-17 22:26:28 -0800

    s

--- Week 9 Wrestling with the Unknown/roving_ruminant_robot_reboot.py
+++ Week 9 Wrestling with the Unknown/roving_ruminant_robot_reboot.py
@@ -1,4 +1,24 @@
 T = int(input())
 
-N, kD, kL, kR, kU = map(int, input().split())
+for _ in range(T):
+    N, kD, kL, kR, kU = map(int, input().split())
+    moves = input()
 
+    if N & 1:
+        print(-1)
+        exit()
+
+    x, y = 0, 0
+    for move in moves:
+        match move:
+            case "R":
+                x += 1
+            case "U":
+                y += 1
+            case "L":
+                x -= 1
+            case "D":
+                y -= 1
+
+    for i in range(16):
+        print()
