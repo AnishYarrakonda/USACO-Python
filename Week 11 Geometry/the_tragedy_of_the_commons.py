26c123ef5531ff0fdf661e30054d7fd3f2f73908
Anish Yarrakonda <number1noob1234@gmail.com>
2026-01-29 17:26:57 -0800

    s

--- Week 11 Geometry/the_tragedy_of_the_commons.py
+++ Week 11 Geometry/the_tragedy_of_the_commons.py
@@ -1,17 +1,12 @@
 N = int(input())
-
 rectangles = [tuple(map(int, input().split())) for _ in range(N)]
 
+e = max(r[0] for r in rectangles)
+f = max(r[1] for r in rectangles)
+g = min(r[2] for r in rectangles)
+h = min(r[3] for r in rectangles)
 
-e = max(rectangles, key=lambda x : x[0])[0]
-f = max(rectangles, key=lambda x : x[1])[1]
-g = min(rectangles, key=lambda x : x[2])[2]
-h = min(rectangles, key=lambda x : x[3])[3]
-
-
-for a, b, c, d in rectangles:
-    if not (c > e and g > a) and (d > f and h > b):
-        print(0)
-        exit()
-
-print(e,f,g,h)
+if e >= g or f >= h:
+    print(0)
+else:
+    print(e, f, g, h)
